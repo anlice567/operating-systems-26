8369065125bbd0aa4012a4efc525281cf30b8bc4
anf123 <152838099+anlice567@users.noreply.github.com>
2026-09-13 17:59:53 +0300

    Update exercise1.sh

--- Lab01/Exercises/exercise1.sh
+++ Lab01/Exercises/exercise1.sh
@@ -1,1 +1,5 @@
-grep -r "gcc" /usr/bin | sort -n -r | tail -n 5 > ~/week01/ex1.txt
+#The command ls gives us the content of the directory /usr/bin
+#The command grep receives the input from the command ls and returns content from /usr/bin with "gcc"
+#The command tail receives the input from the command grep and returns the last 5 items
+#The command sort receives the input from the command tail, sorts the result in reversed order and writes it to the ex1.txt file
+ls /usr/bin | grep "gcc" | tail -n 5 | sort -r > ~/week01/ex1.txt
