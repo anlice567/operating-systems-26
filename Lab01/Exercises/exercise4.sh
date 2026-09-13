3be70be6d6d1f0eecc7bfcedd8e3e51667d76c22
anf123 <152838099+anlice567@users.noreply.github.com>
2026-09-13 16:49:12 +0300

    Create exercise4.sh
    The solution of exercise 4.

---
+++ Lab01/Exercises/exercise4.sh
@@ -0,0 +1,6 @@
+#The command gedit provides a text editor, which allows to create a new file
+gedit ~/week01/main.c
+#The command gcc is responsible for compilation of our C file
+gcc main.c -o ex4
+# ./ is used to run the program
+./ex4
