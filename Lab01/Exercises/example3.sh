0189d358c3979b92f969f21deb747c7a97c935e3
anf123 <152838099+anlice567@users.noreply.github.com>
2026-09-13 21:07:00 +0300

    Create example3.sh

---
+++ Lab01/Exercises/example3.sh
@@ -0,0 +1,10 @@
+#nano allows to create a file and & adds this process to the background
+nano notes &
+#fg sends the process to the foreground
+fg %1
+#gedit provides a text editor to create a file and & adds this process to the background
+gedit notes.txt &
+#shows list of jobs
+jobs
+#the content of notes goes into notes.txt
+cat notes > notes.txt
